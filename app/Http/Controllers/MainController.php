35dfeaa19b160ac5424c5a17a3568bdff9de7889
mmehroz <97299789+mmehroz@users.noreply.github.com>
2022-07-23 23:19:23 +0500

    socket update5

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -293,46 +293,46 @@ class MainController extends Controller
         // socket_close($socket_creation);
 
       // set some variables
-            $host = "localhost";
-            $port = 8080;
-            // don't timeout!
-            // set_time_limit(0);
-            // create socket
-            $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-            // bind socket to port
-            $result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
-            // start listening for connections
-            $result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
-            // accept incoming connections
-            // spawn another socket to handle communication
-            $spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
-            // read client input
-            $input = socket_read($spawn, 1024) or die("Could not read input\n");
-            // clean up input string
-            $input = trim($input);
-            echo "Client Message : ".$input;
-            // reverse client input and send back
-            $output = strrev($input) . "\n";
-            socket_write($spawn, $output, strlen ($output)) or die("Could not write output\n");
-            // close sockets
-            socket_close($spawn);
-            socket_close($socket);
-
-            // $host    = "103.133.133.193";
-            // $port    = 8080;
-            // $message = "Hello Server";
-            // echo "Message To server :".$message;
+            // $host = "localhost";
+            // $port = 8080;
+            // // don't timeout!
+            // // set_time_limit(0);
             // // create socket
             // $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-            // // connect to server
-            // $result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
-            // // send string to server
-            // socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
-            // // get server response
-            // $result = socket_read ($socket, 1024) or die("Could not read server response\n");
-            // echo "Reply From Server  :".$result;
-            // // close socket
+            // // bind socket to port
+            // $result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
+            // // start listening for connections
+            // $result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
+            // // accept incoming connections
+            // // spawn another socket to handle communication
+            // $spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
+            // // read client input
+            // $input = socket_read($spawn, 1024) or die("Could not read input\n");
+            // // clean up input string
+            // $input = trim($input);
+            // echo "Client Message : ".$input;
+            // // reverse client input and send back
+            // $output = strrev($input) . "\n";
+            // socket_write($spawn, $output, strlen ($output)) or die("Could not write output\n");
+            // // close sockets
+            // socket_close($spawn);
             // socket_close($socket);
+
+            $host    = "localhost";
+            $port    = 8080;
+            $message = "Hello Server";
+            echo "Message To server :".$message;
+            // create socket
+            $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+            // connect to server
+            $result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
+            // send string to server
+            socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+            // get server response
+            $result = socket_read ($socket, 1024) or die("Could not read server response\n");
+            echo "Reply From Server  :".$result;
+            // close socket
+            socket_close($socket);
 
         // $sock = stream_socket_client("ws://103.133.133.19:8080/webservice/ocpp/CS12",$error,$errnum,30,STREAM_CLIENT_CONNECT,stream_context_create(null));
         // if (!$sock) {
