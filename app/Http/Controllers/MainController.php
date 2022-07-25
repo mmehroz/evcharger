c2de6f36c99f79aa28c670cacf1b8a3661af2f23
unknown <avidhaus.mehroz@gmail.com>
2022-07-25 17:54:04 +0500

    status4

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -287,7 +287,7 @@ class MainController extends Controller
         //  echo "Hitting the server :".$hello_msg;
         $socket_creation = socket_create(AF_INET, SOCK_STREAM, 0) or die("Unable to create connection with socket\n");
         $server_connect = socket_connect($socket_creation, $IPadress_host , $port_number) or die("Unable to create connection with server\n");
-        socket_write($socket_creation, $hello_msg, strlen($hello_msg)) or die("Unable to send data to the  server\n");
+        socket_write($socket_creation, $data, strlen($data)) or die("Unable to send data to the  server\n");
         $server_connect = socket_read ($socket_creation, 1024) or die("Unable to read response from the server\n");
         echo $server_connect;
         socket_close($socket_creation);
